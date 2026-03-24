eddfb63f87c4cf32c8303a7c47efd7fcdc48881d
Lucas Miranda <104878497+LucasSMiranda@users.noreply.github.com>
2026-03-24 22:50:58 +0000

    Adicionado área de entrada de dados

--- media_nota.c
+++ media_nota.c
@@ -2,8 +2,20 @@
 
 int main()
 {
+    int nota1, nota2, nota3;
+    int media;
 
-    
+    printf("***Programa de cálculo de média***\n");
+
+    printf("Digite sua primeira nota:\n");
+    scanf("%d", &nota1);
+
+    printf("Digite sua segunda nota:\n");
+    scanf("%d", &nota2);
+
+    printf("Digite sua  terceira nota:\n");
+    scanf("%d", &nota3);
+
 
 
     return 0;
