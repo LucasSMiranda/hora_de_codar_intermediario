9ecdcab4d8e2ac5b8aa25f60eafac5fa8bcf0690
Lucas Miranda <104878497+LucasSMiranda@users.noreply.github.com>
2026-03-24 23:10:26 +0000

    Adicionado castin/float para conversão explícita

--- media_nota.c
+++ media_nota.c
@@ -16,7 +16,7 @@ int main()
     printf("Digite sua  terceira nota:\n");
     scanf("%f", &nota3);
 
-    media = (nota1+nota2+nota3)/3;
+    media = (float)(nota1+nota2+nota3)/3;
 
     printf("A média é %.2f\n", media);
 
